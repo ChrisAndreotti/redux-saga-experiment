c5fba50d7e5816653e4d946631908226951675a0
Chris Andreotti <chris.andreotti@gmail.com>
2019-01-12 14:32:04 -0500

    Creates the redux store and wraps the App component in the react-redux Provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
