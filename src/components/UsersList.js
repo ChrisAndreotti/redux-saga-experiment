020f403d3a1e961a6e4db78392e85ba0365002c4
Chris Andreotti <chris.andreotti@gmail.com>
2019-01-20 18:04:34 -0500

    Adds display of users list, alphabetical sorting by first and last name, and delete button

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
 
 const UsersList = ({ users }) => {
     return (
         <ListGroup>
-            {users.map((user) => {
-                return <ListGroupItem key={ user.id }>{ user.firstName }</ListGroupItem>
+            {users.sort((a, b) => {
+                if (a.firstName > b.firstName) {
+                    return 1;
+                } else if (a.firstName < b.firstName) {
+                    return -1;
+                } else if (a.lastName > b.lastName) {
+                    return 1;
+                } else if (a.lastName < b.lastName) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }).map((user) => {
+                return (
+                    <ListGroupItem key={ user.id }>
+                        <section style={{display: 'flex'}}>
+                            <div style={{flexGrow: 1, margin: 'auto 0'}}>
+                                { user.firstName } { user.lastName }
+                            </div>
+                            <div>
+                                <Button outline color="danger">
+                                    Delete
+                                </Button>
+                            </div>
+                        </section>
+                    </ListGroupItem>
+                )
             })}
         </ListGroup>
     )
